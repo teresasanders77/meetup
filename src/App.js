9c036d7dfd18ebd3b5b8addd3abcc4be65298f56
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-09-25 10:10:51 -0600

    Added grid

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents } from './api';
 import { WarningAlert } from './Alert';
+import moment from 'moment';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 
 import './App.css';
 
@@ -20,7 +23,6 @@ class App extends Component {
 
   componentDidMount() {
     this.updateEvents();
-    window.addEventListener('online', this.warningAlert());
   }
 
   updateEvents = (lat, lon, page) => {
@@ -31,14 +33,42 @@ class App extends Component {
       this.setState({ warningText: "" })
     }
     if (lat && lon) {
-      getEvents(lat, lon, this.state.page).then(response => this.setState({ events: response, lat, lon }));
+      getEvents(lat, lon, this.state.page).then(events =>
+        this.setState({ events, lat, lon })
+      );
+    } else if (page) {
+      getEvents(this.state.lat, this.state.lon, page).then(events =>
+        this.setState({ events, page })
+      );
+    } else {
+      getEvents(this.state.lat, this.state.lon, this.state.page).then(events =>
+        this.setState({ events })
+      );
     }
-    else if (page) {
-      getEvents(this.state.lat, this.state.lon, page).then(response => this.setState({ events: response, page }));
+  }
+
+  countEventsOnADate = (date) => {
+    let count = 0;
+    for (let i = 0; i < this.state.events.length; i += 1) {
+      if (this.state.events[i].local_date === date) {
+        count += 1;
+      }
     }
-    else {
-      getEvents(this.state.lat, this.state.lon, this.state.page).then(response => this.setState({ events: response }));
+    return count;
+  }
+
+  getData = () => {
+    const next7Days = []; // Create empty array for the next 7 days 
+    const currentDate = moment(); //Today
+    //Loop 7 times for next 7 days
+    for (let i = 0; i < 7; i += 1) {
+      currentDate.add(1, 'days'); //Add 1 day to current date, current Date changes
+      const dateString = currentDate.format('YYYY-MM-DD'); //Format the date
+      //Use the countEventsOnADate function to count #events on this date
+      const count = this.countEventsOnADate(dateString);
+      next7Days.push({ date: dateString, number: count }); //Add this date and number to the list 
     }
+    return next7Days;
   }
 
   render() {
@@ -46,6 +76,20 @@ class App extends Component {
       <div className="App" >
         <CitySearch updateEvents={this.updateEvents} />
         <NumberOfEvents updateEvents={this.updateEvents} numberOfEvents={this.state.events.length} lat={this.state.lat} lon={this.state.lon} />
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="date" name="date" />
+            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()}
+              fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <WarningAlert text={this.state.warningText} />
         <EventList events={this.state.events} />
       </div>
